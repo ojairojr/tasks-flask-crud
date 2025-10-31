983f7a0762831c334c9608e94eb6eda30aa07085
ojairojr <jairojunior.png@gmail.com>
2025-10-31 11:36:00 -0300

    add class Task e create the method create_task

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from models.task import Task
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, Flask!"
+tasks = []
+task_id_counter = 1
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global task_id_counter
+    data = request.get_json()
+    new_task = Task(id=task_id_counter, title=data['title'], description=data.get('description', ''))
+    task_id_counter += 1
+    tasks.append(new_task)
+    return jsonify(new_task.to_dict()), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
